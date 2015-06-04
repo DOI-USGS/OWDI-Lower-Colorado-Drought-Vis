5512c2b3a0160c1e8e3c23b348a2b6599484b686
Jeff Hollister <jeff.w.hollister@gmail.com>
2015-06-04 13:56:02 -0400

    cleaning up polys from Alan.  still needs to be run

--- scripts/R/water_account.R
+++ scripts/R/water_account.R
@@ -16,6 +16,7 @@ devtools::install_github("jhollist/leafletR")
 library(leafletR)
 library(htmlwidgets)
 library(rgdal)
+library(rgeos)
 library(sp)
 library(miscPackage)
 
@@ -24,47 +25,58 @@ library(miscPackage)
 ################################################################################
 #Used to generate clean topology and generalize.
 #Tolerance of 0.0005 nice trade off between size of files (both under 1Mb) and 
-#accuracy of geography
+#accuracy of geography.
+#Added lower CO boundary from Alan.  had some stray poly's/points.
 #Note: Commented out as it is a one off run.  data are availble in 
 #src_data/CO_WBD
 ################################################################################
-#library(spgrass7)
-#library(raster) 
-#initGRASS("C:\\Program Files (x86)\\GRASS GIS 7.0.1svn",override=TRUE,
-#          home=tempdir())
+library(spgrass6)
+library(raster) 
+initGRASS("C:\\Program Files (x86)\\GRASS GIS 7.0.1svn",override=TRUE,
+          home=tempdir())
 #execGRASS("v.in.ogr",input="src_data//CO_WBD//WBDHU4_14-15.shp", snap=1e-07, 
 #          output="hu4",flags = c("o","overwrite"))
+execGRASS("v.in.ogr",input="src_data//CO_WBD//LC_HUCs_wMexico_20140714.shp", snap=1e-07, 
+        output="lc_huc",flags = c("o","overwrite"))
 #execGRASS("v.generalize",input="hu4",output="hu4_simp",threshold=0.0005,method="douglas",
 #                   flags=c("overwrite","verbose"))
+execGRASS("v.generalize",input="lc_huc",output="lc_huc_simp",threshold=0.0005,method="douglas",
+                   flags=c("overwrite","verbose"))
 
 ################################################################################
 #Aggregate into HUC2.  Using this to make sure linework is the same.  
 #Could be slightly different with hu2 built and generalized separately.
 ################################################################################
 #wbdhu4_lco<-readVECT("hu4_simp")
+lc_huc_simp <- readVECT("lc_huc_simp")
 #HUC2<-substr(as.character(wbdhu4_lco$HUC4),1,2)
 #wbdhu4_lco@data$HUC2<-HUC2
 #wbdhu2_lco<-aggregate(wbdhu4_lco,vars='HUC2')
 
+
+
 ################################################################################
 #Add CRS
 ################################################################################
-#p4s<-"+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+p4s<-"+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
 #proj4string(wbdhu4_lco)<-p4s
 #proj4string(wbdhu2_lco)<-p4s
+proj4string(lc_huc_simp)<-p4s
 
 ################################################################################
 #Write Out to Shape
 ################################################################################
 #writeOGR(wbdhu4_lco,"src_data//CO_WBD","WBDHU4_14-15-clean",driver = "ESRI Shapefile")
 #writeOGR(wbdhu2_lco,"src_data//CO_WBD","WBDHU2_14-15-clean",driver = "ESRI Shapefile")
+writeOGR(lc_huc_simp,"src_data//CO_WBD","lc_huc_simp-clean",driver = "ESRI Shapefile")
 
 
 ################################################################################
 # Get Data set up
 ################################################################################
-wbdhu2_lco<-readOGR("src_data//CO_WBD","WBDHU2_14-15-clean")
-wbdhu4_lco<-readOGR("src_data//CO_WBD","WBDHU4_14-15-clean")
+#wbdhu2_lco<-readOGR("src_data//CO_WBD","WBDHU2_14-15-clean")
+#wbdhu4_lco<-readOGR("src_data//CO_WBD","WBDHU4_14-15-clean")
+lc_huc_simp <-readOGR("src_data//CO_WBD","LC_HUCs_wMexico_20140714")
 wat_acc_examp <- read.csv("src_data//wat_acc_examp.csv")
 wae_brk <- as.numeric(as.character(cut(wat_acc_examp$LastFiveMean,
                                        quantile(wat_acc_examp$LastFiveMean, c(0,0.25,0.75,1)),
@@ -79,42 +91,42 @@ wat_acc_sp <- SpatialPointsDataFrame(wae_coord,data=wae_data,
 ################################################################################
 #rstudio/leaflet implementation
 ################################################################################
-water_account <- leaflet::leaflet() %>% 
-  addPolygons(data=wbdhu4_lco, opacity=0.4,weight = 3) %>%
-  addPolygons(data=wbdhu2_lco, opacity=1 , weight = 4, color="red", fill = FALSE) %>% 
-  addCircleMarkers(data=wat_acc_sp,
-                   color="green",
-                   radius= ~wae_brk, 
-                   popup = ~paste0("<strong>Water User: </strong>", WaterUser, "<br/>",
-                                   "<strong>Consumptive Use 5-year average: </strong>", LastFiveMean))%>%
-  addProviderTiles("Esri.WorldTopoMap") %>%
-  addControl(html="test")
-water_account
+#water_account <- leaflet::leaflet() %>% 
+#  addPolygons(data=wbdhu4_lco, opacity=0.4,weight = 3) %>%
+#  addPolygons(data=wbdhu2_lco, opacity=1 , weight = 4, color="red", fill = FALSE) %>% 
+#  addCircleMarkers(data=wat_acc_sp,
+#                   color="green",
+#                   radius= ~wae_brk, 
+#                   popup = ~paste0("<strong>Water User: </strong>", WaterUser, "<br/>",
+#                                   "<strong>Consumptive Use 5-year average: </strong>", LastFiveMean))%>%
+#  addProviderTiles("Esri.WorldTopoMap") %>%
+#  addControl(html="test")
+#water_account
 
-setwd("public_html//widgets//slide_7/")
-saveWidget(water_account,file="water_accounting.html",
-           selfcontained=FALSE)
+#setwd("public_html//widgets//slide_7/")
+#saveWidget(water_account,file="water_accounting.html",
+#           selfcontained=FALSE)
 
 ################################################################################
 #leafletR implementation
 ################################################################################
-huc2_gjson <- toGeoJSON(wbdhu2_lco,dest="play")
-wae_gjson <- toGeoJSON(wat_acc_sp)
-dat<-list(huc2_gjson,wae_gjson)
-huc2_sty <- styleSingle(col="red")#, lwd=5, alpha=0.5)
-huc4_sty <- styleSingle(col="blue") #,lwd=2,alpha=0.5,fill="blue",fill.alpha=0.5)
+lc_huc_gjson <- toGeoJSON(lc_huc_simp,dest="src_data/CO_WBD")
+wae_gjson <- toGeoJSON(wat_acc_sp,dest="src_data/CO_WBD")
+
+dat<-list(lc_huc_gjson,wae_gjson)
+lc_huc_sty <- styleSingle(col="red")#, lwd=5, alpha=0.5)
 #wae_brk <- as.numeric(quantile(wat_acc_examp$LastFiveMean, c(0,0.25,0.75,1)))
 #wae_sty <- styleGrad("LastFiveMean",
 #                     breaks= c(0,5000,10000), 
 #                     style.par = "rad",
 #                     style.val=c(5,20))
 wae_sty <- styleSingle(col="green")
-sty<-list(huc2_sty,wae_sty)
+sty<-list(lc_huc_sty,wae_sty)
 water_account <- leafletR::leaflet(data=dat,
                                    base.map=list("osm","mqsat"),
                                    style=sty, 
                                    popup = list("HUC2",c("WaterUser","LastFiveMean")),
-                                   dest="play",
-                                   incl.data = T,
+                                   dest="public_html/widgets/slide_7/",
+                                   incl.data = FALSE,
                                    controls=c("all"))
 water_account
