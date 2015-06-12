b6e2b1c51b6bf598d8a74a59ba80a63d07d2dd2e
Luke Winslow <lwinslow@usgs.gov>
2015-06-12 11:34:59 -0500

    start adding scripts to run, not sure if they will all stay

--- scripts/update_all.R
+++ scripts/update_all.R
@@ -5,12 +5,14 @@ if(basename(origin) != 'OWDI-Lower-Colorado-Drought-Vis'){
 	stop('Check your location, you should be at the root of the  OWDI-Lower-Colorado-Drought-Viz repo')
 }
 
+##Start running the update scripts for each figure. Run local=TRUE in an attempt 
+##	to prevent scripts from stepping on each other's toes
 
 source('scripts/R/meadHistoricalAndProjection.R', local=TRUE)
-
 
 source('scripts/R/natural_flow_history.R', local=TRUE)
 
 source('scripts/R/water_account.R', local=TRUE)
 
+source('scripts/R/supply_vs_use_animation.R', local=TRUE)
 
