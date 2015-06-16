cbd044c56f09c643f290299069530c659c552aed
Jordan S Read <jread@usgs.gov>
2015-06-16 10:50:13 -0500

    trigger functions

--- scripts/R/build_ecmascript.R
+++ scripts/R/build_ecmascript.R
@@ -6,7 +6,28 @@ build_ecmascript <- function(){
                'svgDocument.drawRiver = this.drawRiver;}',
                '}',
                'function drawRiver(){',
-               'document.getElementById("colorado-river-draw").beginElement();}')
+               'document.getElementById("colorado-river-draw").beginElement();}',
+               'function drawBasin(){',
+               'document.getElementById("colorado-basin-draw").beginElement();}',
+               'function drawPictogram(){',
+               'document.getElementById("pictogram-topfive-draw").beginElement();}',
+               
+               'function removeGreyStates(){',
+               'document.getElementById("remove-grey-states").beginElement();}',
+               'function removeRiver(){',
+               'document.getElementById("remove-river").beginElement();',
+               'document.getElementById("remove-basin").beginElement();}',
+               'function removePictogram(){',
+               'document.getElementById("remove-pictogram").beginElement();}',
+               'function moveStates(){',
+               'document.getElementById("California-move").beginElement();',
+               'document.getElementById("Nevada-move").beginElement();',
+               'document.getElementById("Arizona-move").beginElement();',
+               'document.getElementById("Nevada-move").beginElement();',
+               'document.getElementById("mexico-move").beginElement();',
+               'document.getElementById("Mexico-stroke").beginElement();',
+               'document.getElementById("Mexico-scale").beginElement();}'
+               )
   
   
 }
