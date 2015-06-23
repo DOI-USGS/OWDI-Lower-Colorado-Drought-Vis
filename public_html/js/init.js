49edb1a4f993b79de7aa1621be6ff07e9f603b41
Ivan Suftin <isuftin@usgs.gov>
2015-06-23 09:47:48 -0500

    Reworked the js initialization so we can use named containers/sections instead of numbered

--- public_html/js/init.js
+++ public_html/js/init.js
@@ -1,31 +1,56 @@
+/* jshint strict: true */
+/* global $ */
+/* global ScrollMagic */
+/* global owdiDrought */
+/* global Handlebars */
 $(document).ready(function() {
+    "use strict";
     window.owdiDrought = window.owdiDrought || {};
     window.owdiDrought.SMController = new ScrollMagic.Controller();
 
+
     (function() {
-        var mkTemplate = function(section) {
-            return $.ajax({
-                url: "sections/" + section + "/" + section + ".html",
-                success: function(data) {
-                    var template = Handlebars.compile(data);
-                    $("#" + section).html(template());
+
+        // For every container, find the sections it holds. Then for each section,
+        // use the section's HTML id attribtue to load the template. Once the template
+        // is loaded, apply it to the specific container->section. This can be further
+        // refactored to include subsections within each section. Some sections have a
+        // function that should be triggered after loading it. 
+        var containers = $(".container"),
+            loadTemplate = function(containerId, sectionId) {
+                return $.ajax({
+                    url: "sections/" + sectionId + "/" + sectionId + ".html",
+                    context: {
+                        containerId: containerId,
+                        sectionId: sectionId
+                    }
+                });
+            },
+            applyTemplate = function(data) {
+                var template = Handlebars.compile(data);
+                $("#" + this.containerId + " > #" + this.sectionId).html(template());
+            },
+            initializeSections = function () {
+                switch (this.sectionId) {
+                    case "section5":
+                        owdiDrought.createDygraph("natural-flow-graph");
+                        break;
+                    case "section13":
+                        owdiDrought.slider.sliderInit();
+                        break;
                 }
-            });
-        }
+            };
 
-        for (var sIdx = 1; sIdx < 17; sIdx++) {
-            var ajax = mkTemplate("section" + sIdx);
+        for (var cIdx = 0; cIdx < containers.length; cIdx++) {
+            var $container = $(containers[cIdx]),
+                sections = $container.find(".section");
+            for (var sIdx = 0; sIdx < sections.length; sIdx++) {
+                var $section = $(sections[sIdx]),
+                    containerId = $container.attr("id"),
+                    sectionId = $section.attr("id");
+                var loadTemplateDeferred = loadTemplate(containerId, sectionId);
 
-            if (sIdx === 5) {
-                ajax.done(function(data) {
-                    owdiDrought.createDygraph("natural-flow-graph");
-                })
-            }
-
-            if (sIdx === 13) {
-                ajax.done(function(data) {
-                    owdiDrought.slider.sliderInit();
-                })
+                loadTemplateDeferred.done(applyTemplate, initializeSections);
             }
         }
     })();
