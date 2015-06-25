b9c01fb67af4b427bb56fc659dc23b625d86d9c7
Ivan Suftin <isuftin@usgs.gov>
2015-06-25 08:22:59 -0500

    js cleanup and unused js removal

--- public_html/js/before-after.js
+++ public_html/js/before-after.js
@@ -1,88 +1,114 @@
-owdiDrought.slider = {};
-owdiDrought.slider.sliderInit = function(){
-	
-    //check if the .cd-image-container is in the viewport 
-    //if yes, animate it
-    owdiDrought.slider.checkPosition($('.cd-image-container'));
-    $(window).on('scroll', function(){
-        owdiDrought.slider.checkPosition($('.cd-image-container'));
-    });
-    
-    //make the .cd-handle element draggable and modify .cd-resize-img width according to its position
-    $('.cd-image-container').each(function(){
-        var actual = $(this);
-        owdiDrought.slider.drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual, actual.find('.cd-image-label[data-type="original"]'), actual.find('.cd-image-label[data-type="modified"]'));
-    });
+/* global owdiDrought */
+/* global $ */
+(function() {
+    "use strict";
+    owdiDrought.slider = {};
+    owdiDrought.slider.sliderInit = function() {
+        var $cdImageContainer = $(".cd-image-container"),
+            cdImageLabelClassname = ".cd-image-label",
+            cdResizeImgClassName = ".cd-resize-img";
+            
+        //check if the .cd-image-container is in the viewport 
+        //if yes, animate it
+        owdiDrought.slider.checkPosition($cdImageContainer);
+        $(window).on("scroll", function() {
+            owdiDrought.slider.checkPosition($cdImageContainer);
+        });
 
-    //upadate images label visibility
-    $(window).on('resize', function(){
-        $('.cd-image-container').each(function(){
+        //make the .cd-handle element draggable and modify .cd-resize-img width according to its position
+        $cdImageContainer.each(function() {
             var actual = $(this);
-            owdiDrought.slider.updateLabel(actual.find('.cd-image-label[data-type="modified"]'), actual.find('.cd-resize-img'), 'left');
-            owdiDrought.slider.updateLabel(actual.find('.cd-image-label[data-type="original"]'), actual.find('.cd-resize-img'), 'right');
+            owdiDrought.slider.drags(
+                actual.find(".cd-handle"),
+                actual.find(cdResizeImgClassName),
+                actual,
+                actual.find(cdImageLabelClassname + "[data-type='original']"),
+                actual.find(cdImageLabelClassname + "[data-type='modified']"));
         });
-    });
-}
 
-owdiDrought.slider.checkPosition = function(container) {
-    container.each(function(){
-        var actualContainer = $(this);
-        if( $(window).scrollTop() + $(window).height()*0.5 > actualContainer.offset().top) {
-            actualContainer.addClass('is-visible');
-        }
-    });
-}
-
-//draggable funtionality - credits to http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
-owdiDrought.slider.drags = function(dragElement, resizeElement, container, labelContainer, labelResizeElement) {
-    dragElement.on("mousedown vmousedown", function(e) {
-        dragElement.addClass('draggable');
-        resizeElement.addClass('resizable');
-
-        var dragWidth = dragElement.outerWidth(),
-            xPosition = dragElement.offset().left + dragWidth - e.pageX,
-            containerOffset = container.offset().left,
-            containerWidth = container.outerWidth(),
-            minLeft = containerOffset + 10,
-            maxLeft = containerOffset + containerWidth - dragWidth - 10;
-        
-        dragElement.parents().on("mousemove vmousemove", function(e) {
-            leftValue = e.pageX + xPosition - dragWidth;
-            
-            //constrain the draggable element to move inside his container
-            if(leftValue < minLeft ) {
-                leftValue = minLeft;
-            } else if ( leftValue > maxLeft) {
-                leftValue = maxLeft;
-            }
-
-            widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
-            
-            $('.draggable').css('left', widthValue).on("mouseup vmouseup", function() {
-                $(this).removeClass('draggable');
-                resizeElement.removeClass('resizable');
+        //upadate images label visibility
+        $(window).on("resize", function() {
+            $cdImageContainer.each(function() {
+                var actual = $(this);
+                owdiDrought.slider.updateLabel(actual.find(cdImageLabelClassname + "[data-type='modified']"), actual.find(cdResizeImgClassName), "left");
+                owdiDrought.slider.updateLabel(actual.find(cdImageLabelClassname + "[data-type='original']"), actual.find(cdResizeImgClassName), "right");
             });
-
-            $('.resizable').css('width', widthValue); 
-
-            owdiDrought.slider.updateLabel(labelResizeElement, resizeElement, 'left');
-            owdiDrought.slider.updateLabel(labelContainer, resizeElement, 'right');
-            
-        }).on("mouseup vmouseup", function(e){
-            dragElement.removeClass('draggable');
-            resizeElement.removeClass('resizable');
         });
-        e.preventDefault();
-    }).on("mouseup vmouseup", function(e) {
-        dragElement.removeClass('draggable');
-        resizeElement.removeClass('resizable');
-    });
-}
+    };
 
-owdiDrought.slider.updateLabel = function(label, resizeElement, position) {
-    if(position == 'left') {
-        ( label.offset().left + label.outerWidth() < resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
-    } else {
-        ( label.offset().left > resizeElement.offset().left + resizeElement.outerWidth() ) ? label.removeClass('is-hidden') : label.addClass('is-hidden') ;
-    }
-}
+    owdiDrought.slider.checkPosition = function(container) {
+        container.each(function() {
+            var actualContainer = $(this);
+            if ($(window).scrollTop() + $(window).height() * 0.5 > actualContainer.offset().top) {
+                actualContainer.addClass("is-visible");
+            }
+        });
+    };
+
+    //draggable funtionality - credits to http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
+    owdiDrought.slider.drags = function(dragElement, resizeElement, container, labelContainer, labelResizeElement) {
+        dragElement.on("mousedown vmousedown", function(e) {
+            var vmouseAndMouseUp = "mouseup vmouseup",
+                draggable = "draggable",
+                resizable = "resizable";
+
+            dragElement.addClass(draggable);
+            resizeElement.addClass(resizable);
+
+            var dragWidth = dragElement.outerWidth(),
+                xPosition = dragElement.offset().left + dragWidth - e.pageX,
+                containerOffset = container.offset().left,
+                containerWidth = container.outerWidth(),
+                minLeft = containerOffset + 10,
+                maxLeft = containerOffset + containerWidth - dragWidth - 10;
+
+            dragElement.parents().on("mousemove vmousemove", function(e) {
+                var leftValue = e.pageX + xPosition - dragWidth;
+
+                //constrain the draggable element to move inside his container
+                if (leftValue < minLeft) {
+                    leftValue = minLeft;
+                } else if (leftValue > maxLeft) {
+                    leftValue = maxLeft;
+                }
+
+                var widthValue = (leftValue + dragWidth / 2 - containerOffset) * 100 / containerWidth + "%";
+
+                $("." + draggable).css("left", widthValue).on(vmouseAndMouseUp, function() {
+                    $(this).removeClass(draggable);
+                    resizeElement.removeClass(resizable);
+                });
+
+                $("." + resizable).css("width", widthValue);
+
+                owdiDrought.slider.updateLabel(labelResizeElement, resizeElement, "left");
+                owdiDrought.slider.updateLabel(labelContainer, resizeElement, "right");
+
+            }).on(vmouseAndMouseUp, function() {
+                dragElement.removeClass(draggable);
+                resizeElement.removeClass(resizable);
+            });
+            e.preventDefault();
+        }).on(vmouseAndMouseUp, function() {
+            dragElement.removeClass(draggable);
+            resizeElement.removeClass(resizable);
+        });
+    };
+
+    owdiDrought.slider.updateLabel = function(label, resizeElement, position) {
+        var isHiddenLabel = "is-hidden";
+        if (position == "left") {
+            if (label.offset().left + label.outerWidth() < resizeElement.offset().left + resizeElement.outerWidth()) {
+                label.removeClass(isHiddenLabel);
+            } else {
+                label.addClass(isHiddenLabel);
+            }
+        } else {
+            if (label.offset().left > resizeElement.offset().left + resizeElement.outerWidth()) {
+                label.removeClass(isHiddenLabel);
+            } else {
+                label.addClass(isHiddenLabel);
+            }
+        }
+    };
+})();
