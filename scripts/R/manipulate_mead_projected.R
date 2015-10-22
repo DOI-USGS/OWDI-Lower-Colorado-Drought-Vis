7bd8901dbc82d706baf490abb231eaf21a652479
Jordan S Read <jread@usgs.gov>
2015-10-22 11:49:57 -0500

    breaking up w/ form.factor and language

--- scripts/R/manipulate_mead_projected.R
+++ scripts/R/manipulate_mead_projected.R
@@ -13,6 +13,7 @@ mead_projected_svg <- function(data, unit = 'Imperial', form.factor='desktop', l
   add_ecmascript(svg_nd, text = ecmascript_mead_proj())
   
   add_blocks(svg_nd, form.factor, language, fig, axes)
+  add_annotations(svg_nd, form.factor, language, fig, axes)
   g_id <- newXMLNode('g', parent = svg_nd, attrs = c('id' = "axes", class='label'))
   
   
@@ -30,10 +31,11 @@ mead_projected_svg <- function(data, unit = 'Imperial', form.factor='desktop', l
 
 get_axes <- function(data, form.factor, language){
   unit <- c('es'='meters', 'en'='feet')
-  x.ticks = c('desktop'=8, 'mobile'=4)
+  x.ticks = c('desktop'=8, 'mobile'=6)
+  x.label = c('desktop'='Year', 'mobile'='')
   y.label = c('desktop'=paste0("Elevation of Lake Mead (",unit[[language]],")"), 'mobile'=paste0("Elevation of Lake Mead (",unit[[language]],")"))
   
-  y.ticks = list('es'=seq(1000*conv,1225*conv,10), 'en'=seq(1000,1225,25))
+  y.ticks = list('es'=list('desktop'=seq(1000*conv,1225*conv,10),'mobile'=seq(1000*conv,1225*conv,50)), 'en'=list('desktop'=seq(1000,1225,25), 'mobile'=seq(1000,1225,50)))
   y.lim = list('es'=c(990*conv,1229*conv), 'en'=c(990,1229))
   
   get_ticks <- function(x, n){
@@ -56,36 +58,54 @@ get_axes <- function(data, form.factor, language){
   
   axes <- list('tick_len' = 5,
                'y_label' = y.label[[form.factor]],
-               'x_label' = "Year",
-               'y_ticks' = y.ticks[[language]],
-               'y_tk_label' = y.ticks[[language]],
-               'x_ticks' = seq(xMinR,xMaxR,2),
-               'x_tk_label' = seq(xMinR,xMaxR,2),
+               'x_label' = x.label[[form.factor]],
+               'y_ticks' = y.ticks[[language]][[form.factor]],
+               'y_tk_label' = y.ticks[[language]][[form.factor]],
+               'x_ticks' = get_ticks(c(xMinR,xMaxR), x.ticks[[form.factor]]),
+               'x_tk_label' = get_ticks(c(xMinR,xMaxR), x.ticks[[form.factor]]),
                'y_lim' = y.lim[[language]],
                'x_lim' = c(xMinR-.25,xMaxR+maxMonth/12)) 
 
   return(axes)
 }
 
+
+get_heights = function(axes, fig){
+  x = c()
+  x$peak = floor(dinosvg:::tran_y(axes$y_lim[2], axes, fig))
+  x$flood = floor(dinosvg:::tran_y(1219.6, axes, fig))
+  x$surplus = floor(dinosvg:::tran_y(1200, axes, fig))
+  x$normal = floor(dinosvg:::tran_y(1145, axes, fig))
+  x$shortage = floor(dinosvg:::tran_y(1075, axes, fig))
+  x$bottom = floor(dinosvg:::tran_y(axes$y_lim[1], axes, fig))
+  
+  return(x)
+}
 add_legend <- function(g_id, form.factor, language, fig){
+  
+  l_bmp <- 15
+  line_ln <- 15
+  x1 = fig$margins[2]+l_bmp+line_ln
+  y1 = fig$h-fig$margins[1]-45
   
   leg_id <- newXMLNode("g", 'parent' = g_id,
                        attrs = c('id' = "legend", 
                                  class="label", 'alignment-baseline' = "central"))
   
-  newXMLNode("text", parent = leg_id, newXMLTextNode(' '),
-             attrs = c(id="date_text", x=fig$margins[2], y=fig$h-fig$margins[1],dy="-0.6em", dx="0.4em",class='legend'))
-  newXMLNode("text", parent = leg_id, newXMLTextNode(' '),
-             attrs = c(id="elev_text", x=fig$margins[2], y=fig$h-fig$margins[1], dy="0.4em", dx="0.4em",fill = '#000000', class='legend'))
-  newXMLNode("text", parent = leg_id, newXMLTextNode(' '),
-             attrs = c(id="model_text", x=fig$margins[2], y=fig$h-fig$margins[1],dy="1.6em",  dx="0.4em",fill = '#000000',class='legend'))
-  newXMLNode('line',parent=leg_id, attrs=c(id='legend-line', 'x1'=fig$margins[2]+15,'x2'=fig$margins[2], y1=fig$h-fig$margins[1], y2= fig$h-fig$margins[1],fill = 'none', 
+  newXMLNode('line',parent=leg_id, attrs=c(id='legend-line', 'x1'=x1-line_ln,'x2'=x1, y1=y1, y2=y1,fill = 'none', 
                                            style =sprintf("stroke:%s;stroke-width:%s;stroke-linejoin:round;stroke-linecap:round",
                                                           supply_col,line_width),visibility='hidden'))
+  newXMLNode("text", parent = leg_id, newXMLTextNode(' '),
+             attrs = c(id="date_text", x=x1, y=y1,dy="-0.6em", dx="0.4em",class='legend'))
+  newXMLNode("text", parent = leg_id, newXMLTextNode(' '),
+             attrs = c(id="elev_text", x=x1, y=y1, dy="0.4em", dx="0.4em",fill = '#000000', class='legend'))
+  newXMLNode("text", parent = leg_id, newXMLTextNode(' '),
+             attrs = c(id="model_text", x=x1, y=y1,dy="1.6em",  dx="0.4em",fill = '#000000',class='legend'))
+  
 }
 
 get_fig <- function(form.factor){
-  fig.dims <- list('mobile'=list('w' = 500,'h' = 400, 'margins' = c(40,60,10, 60)),
+  fig.dims <- list('mobile'=list('w' = 500,'h' = 400, 'margins' = c(60,80,10, 10)),
                    'desktop'=list('w' = 960,'h' = 600,'margins' = c(50,100,10, 100)))
   fig <- fig.dims[[form.factor]]
   
@@ -94,60 +114,66 @@ get_fig <- function(form.factor){
   return(fig)
 }
 
-read_mead_projected <- function(){
+
+
+add_annotations <- function(svg_nd, form.factor, language, fig, axes){
+  if (form.factor == 'desktop'){
+    x = get_heights(axes, fig)
+    g_id = newXMLNode("g", parent = svg_nd, attrs=c(class='hidden', id='condition-markers'))
+    newXMLNode("text", parent = g_id, newXMLTextNode('Flood Control Conditions'),
+               attrs = c(x = fig$px_lim$x[2], y = mean(c(x$surplus,x$flood)),dy="0.5em", dx="0.4em",class='small-text'))
+    newXMLNode("text", parent = g_id, newXMLTextNode('Surplus Conditions'),
+               attrs = c(x = fig$px_lim$x[2], y = mean(c(x$normal,x$surplus)),dy="0.5em", dx="0.4em",class='small-text'))
+    newXMLNode("text", parent = g_id, newXMLTextNode('Normal Conditions'),
+               attrs = c(x = fig$px_lim$x[2], y = mean(c(x$shortage,x$normal)),dy="0.5em", dx="0.4em",class='small-text'))
+    newXMLNode("text", parent = g_id, newXMLTextNode('Shortage Conditions'),
+               attrs = c(x = fig$px_lim$x[2], y = mean(c(x$bottom,x$shortage)),dy="0.5em", dx="0.4em",class='small-text')) 
+  } else {
+    g_id = newXMLNode("g", parent = svg_nd, attrs=c(class='hidden', id='condition-markers'))
+    x1 = fig$px_lim$x[1]
+    x2 = fig$px_lim$x[2]
+    x.rng = x2-x1
+    bins = 4
+    spaces = bins-1
+    space.w = 30
+    bin.w = (x.rng-spaces*space.w)/bins
+    bin.h = 20
+    y.off = 34
+    labels = c('Flood Control', "Surplus",'Normal','Shortage')
+    opacity = c(.8, .7, .6, .4)
+    for (i in seq_len(length(labels))){
+      newXMLNode('rect',parent = g_id, attrs = c(x = x1+(i-1)*(bin.w+space.w), y = fig$h-fig$margins[3]-fig$margins[1]+y.off,
+                                                 width=bin.w, height=bin.h,
+                                                 fill='#0066CC',opacity=opacity[i]))
+      newXMLNode("text", parent = g_id, newXMLTextNode(labels[i]),
+                 attrs = c(x = x1+bin.w/2+(i-1)*(bin.w+space.w), y = fig$h-fig$margins[3]-fig$margins[1]+bin.h+y.off, dy="1.0em", class='small-text', 'text-anchor'='middle'))
+    }
+    
+  }
   
-  # -- from Alan Butler's code --
-  hData <- read.csv('src_data/MeadHistorical.csv', stringsAsFactors = F)
-  # add RunType column to historical data
-  hData$RunType <- 99
-  modData <- read.csv('src_data/Mead24MSResults.csv', stringsAsFactors = F)
-  meadData <- rbind(hData, modData)
-  
-  # limit to only Mead, Pool Elevation
-  meadData <- dplyr::filter(meadData, Object.Name == 'Mead', Slot.Name == 'Pool Elevation')
-  
-  
-  # determine the y-axis limits
-  
-  
-  meadData$posDate = as.POSIXct(meadData$Timestep, format='%m/%d/%Y')
-  return(meadData)
   
 }
 
 add_blocks <- function(svg_nd, form.factor, language, fig, axes){
   
-  peak = floor(dinosvg:::tran_y(axes$y_lim[2], axes, fig))
-  flood = floor(dinosvg:::tran_y(1219.6, axes, fig))
-  surplus = floor(dinosvg:::tran_y(1200, axes, fig))
-  normal = floor(dinosvg:::tran_y(1145, axes, fig))
-  shortage = floor(dinosvg:::tran_y(1075, axes, fig))
-  bottom = floor(dinosvg:::tran_y(axes$y_lim[1], axes, fig))
-  
-  newXMLNode('rect',parent = svg_nd, attrs = c(id='peak',x = fig$px_lim$x[1], y = peak,
-                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=flood-peak+0.5,
+
+  x = get_heights(axes, fig)
+  newXMLNode('rect',parent = svg_nd, attrs = c(id='peak',x = fig$px_lim$x[1], y = x$peak,
+                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=x$flood-x$peak+0.2,
                                                fill='#0066CC',opacity=0.9, class='level-fill'))
-  newXMLNode('rect',parent = svg_nd, attrs = c(id='flood',x = fig$px_lim$x[1], y = flood,
-                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=surplus-flood+0.5,
+  newXMLNode('rect',parent = svg_nd, attrs = c(id='flood',x = fig$px_lim$x[1], y = x$flood,
+                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=x$surplus-x$flood+0.2,
                                                fill='#0066CC', opacity=0.8, class='level-fill'))
-  newXMLNode('rect',parent = svg_nd, attrs = c(id='surplus',x = fig$px_lim$x[1], y = surplus,
-                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=normal-surplus+0.5,
+  newXMLNode('rect',parent = svg_nd, attrs = c(id='surplus',x = fig$px_lim$x[1], y = x$surplus,
+                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=x$normal-x$surplus+0.2,
                                                fill='#0066CC',opacity=0.7, class='level-fill'))
-  newXMLNode('rect',parent = svg_nd, attrs = c(id='normal',x = fig$px_lim$x[1], y = normal,
-                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=shortage-normal+0.5,
+  newXMLNode('rect',parent = svg_nd, attrs = c(id='normal',x = fig$px_lim$x[1], y = x$normal,
+                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=x$shortage-x$normal+0.2,
                                                fill='#0066CC',opacity=0.6, class='level-fill'))
-  newXMLNode('rect',parent = svg_nd, attrs = c(id='shortage',x = fig$px_lim$x[1], y = shortage,
-                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=bottom-shortage+0.5,
+  newXMLNode('rect',parent = svg_nd, attrs = c(id='shortage',x = fig$px_lim$x[1], y = x$shortage,
+                                               width=fig$px_lim$x[2]-fig$px_lim$x[1], height=x$bottom-x$shortage+0.2,
                                                fill='#0066CC',opacity=0.4, class='level-fill'))
-  g_id = newXMLNode("g", parent = svg_nd, attrs=c(class='hidden', id='condition-markers'))
-  newXMLNode("text", parent = g_id, newXMLTextNode('Flood Control Conditions'),
-             attrs = c(x = fig$px_lim$x[2], y = mean(c(surplus,flood)),dy="0.5em", dx="0.4em",class='small-text'))
-  newXMLNode("text", parent = g_id, newXMLTextNode('Surplus Conditions'),
-             attrs = c(x = fig$px_lim$x[2], y = mean(c(normal,surplus)),dy="0.5em", dx="0.4em",class='small-text'))
-  newXMLNode("text", parent = g_id, newXMLTextNode('Normal Conditions'),
-             attrs = c(x = fig$px_lim$x[2], y = mean(c(shortage,normal)),dy="0.5em", dx="0.4em",class='small-text'))
-  newXMLNode("text", parent = g_id, newXMLTextNode('Shortage Conditions'),
-             attrs = c(x = fig$px_lim$x[2], y = mean(c(bottom,shortage)),dy="0.5em", dx="0.4em",class='small-text'))
+  
 }
 
 add_lines <- function(g_id, data, form.factor, language){
@@ -155,6 +181,8 @@ add_lines <- function(g_id, data, form.factor, language){
   
   axes = get_axes(data, form.factor, language)
   fig = get_fig(form.factor)
+  y_offset <- c('desktop'=fig$px_lim$y[1]-20, 'mobile'=fig$px_lim$y[2]+20)
+  t_anc = c('desktop'='begin','mobile'='end')
   
   posDate = data$posDate
   
@@ -179,15 +207,17 @@ add_lines <- function(g_id, data, form.factor, language){
                      style =sprintf("stroke:#FFFFFF;stroke-width:%s;stroke-dasharray:5,2",
                                     1.5))
   
-  y_offset <- fig$px_lim$y[1]-20
   x_offset <- c(x[is.most][1] -4, x[is.most][1] + 18)
   
+  
   newXMLNode("text", parent = g_id, newXMLTextNode('Historical'),
-             attrs = c(id="Historical-marker", class='hidden',
-                       'transform'=sprintf("rotate(-90,%1.1f,%1.1f),translate(%1.1f,%1.1f)",x_offset[1],y_offset,x_offset[1],y_offset)))
+             attrs = c(id="Historical-marker", class='hidden', 'text-anchor'=t_anc[[form.factor]],
+                       'transform'=sprintf("rotate(-90,%1.1f,%1.1f),translate(%1.1f,%1.1f)",
+                                           x_offset[1],y_offset[[form.factor]],x_offset[1],y_offset[[form.factor]])))
   newXMLNode("text", parent = g_id, newXMLTextNode('Projected'),
-             attrs = c(id="Projected-marker", class='hidden',
-                       'transform'=sprintf("rotate(-90,%1.1f,%1.1f),translate(%1.1f,%1.1f)",x_offset[2],y_offset,x_offset[2],y_offset)))
+             attrs = c(id="Projected-marker", class='hidden','text-anchor'=t_anc[[form.factor]],
+                       'transform'=sprintf("rotate(-90,%1.1f,%1.1f),translate(%1.1f,%1.1f)",
+                                           x_offset[2],y_offset[[form.factor]],x_offset[2],y_offset[[form.factor]])))
   
   dinosvg:::linepath(g_id, x[is.hist],y[is.hist],fill = 'none', 
                      style =sprintf("stroke:%s;stroke-width:%s;stroke-linejoin:round;stroke-linecap:round",
@@ -223,5 +253,27 @@ add_lines <- function(g_id, data, form.factor, language){
                          onmouseover="highlightEle(evt,'0.1')",
                          onmouseout="highlightEle(evt,'0.0')"))
   }
+  
+}
+
+
+read_mead_projected <- function(){
+  
+  # -- from Alan Butler's code --
+  hData <- read.csv('src_data/MeadHistorical.csv', stringsAsFactors = F)
+  # add RunType column to historical data
+  hData$RunType <- 99
+  modData <- read.csv('src_data/Mead24MSResults.csv', stringsAsFactors = F)
+  meadData <- rbind(hData, modData)
+  
+  # limit to only Mead, Pool Elevation
+  meadData <- dplyr::filter(meadData, Object.Name == 'Mead', Slot.Name == 'Pool Elevation')
+  
+  
+  # determine the y-axis limits
+  
+  
+  meadData$posDate = as.POSIXct(meadData$Timestep, format='%m/%d/%Y')
+  return(meadData)
   
 }
