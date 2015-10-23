2e243b7268c966972e6322d741f4e4ba40d6168e
Jordan Walker <jiwalker@usgs.gov>
2015-10-23 13:00:49 -0500

    Fixing up translation scripts to properly render the html.

---
+++ scripts/R/read_translation.R
@@ -0,0 +1,16 @@
+library(xml2)
+
+translation <- list()
+
+lookupTranslation <- function(key, lang) {
+  return(translation[[lang]][[key]])
+}
+
+for (lang in c("en", "es")) {
+  # read back xml file
+  xml <- as_list(read_xml(paste0("src_data/full_text.", lang, ".xml")))
+  xml <- lapply(xml, function(x){if (is.list(x))unlist(x)})
+  xml <- xml[!sapply(xml, is.null)]
+  
+  translation[[lang]] <- xml
+}
--- scripts/R/render_html.R
+++ scripts/R/render_html.R
@@ -1,13 +1,12 @@
 library(whisker)
+source("scripts/R/read_translation.R")
 
 src <- "src_data/html"
-infile <- "src_data/full_text.tsv"
 template <- NULL
 partials <- list()
-translation <- read.table(infile, sep="\t", stringsAsFactors = FALSE, header = TRUE, quote = "\"")
-tr <- translation$value
-names(tr) <- translation$key
-data <- list(tr=tr, lang="en", locale="en_US")
+
+locale <- list(en="en_US", es="es_MX")
+
 for (file in dir(src)) {
   name <- strsplit(file, ".mustache", fixed=TRUE)[[1]]
   if (name == "index") {
@@ -16,5 +15,8 @@ for (file in dir(src)) {
     partials[[name]] <- readLines(paste(src, file, sep="/"))
   }
 }
-
-cat(whisker.render(template = template, partials = partials, data = data), file="public_html/en/index.html")
+for (lang in c("en", "es")) {
+  data <- list(tr=translation[[lang]], lang=lang, locale=locale[[lang]])
+  cat(whisker.render(template = template, partials = partials, data = data),
+      file=paste0("public_html/", lang, "/index.html"))
+}
